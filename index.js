f833afdc79a6a1004ea5af4b31c1d5650c988560
raicem <unalancem@gmail.com>
2019-03-05 22:19:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+let mix = require("laravel-mix");
+
+class ReactTypescript {
+  constructor() {
+    this.toCompile = [];
+  }
+
+  dependencies() {
+    return [
+      "@babel/preset-typescript",
+      "@babel/preset-react",
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread",
+      "typescript"
+    ];
+  }
+
+  register(entry, output) {
+    entry = [].concat(entry).map(file => new File(file));
+    output = new File(output);
+
+    this.toCompile.push({ entry, output });
+  }
+
+  webpackEntry(entry) {
+    this.toCompile.forEach(js => {
+      entry.addFromOutput(
+        js.entry.map(file => file.path()),
+        js.output,
+        js.entry[0]
+      );
+    });
+  }
+
+  webpackRules() {
+    return [
+      {
+        test: /\.ts|.tsx?$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: Config.babel()
+          }
+        ]
+      }
+    ];
+  }
+
+  webpackConfig(webpackConfig) {
+    webpackConfig.resolve.extensions.push(".ts", ".tsx");
+  }
+
+  babelConfig() {
+    return {
+      presets: ["@babel/preset-typescript", "@babel/preset-react"],
+      plugins: [
+        "@babel/proposal-class-properties",
+        "@babel/proposal-object-rest-spread"
+      ]
+    };
+  }
+}
+
+mix.extend("reactTypescript", new ReactTypescript());
